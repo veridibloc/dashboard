c093696f9de53e41759bcd0576b372737fd8274a
ohager <oliver@devbutze.com>
2024-10-12 02:03:53 -0300

    fix: another trial to fix prisma

--- lib/server/prisma.ts
+++ lib/server/prisma.ts
@@ -4,15 +4,9 @@ declare global {
   var prisma: PrismaClient; // This must be a `var` and not a `let / const`
 }
 
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+const prisma = new PrismaClient();
+if (!global.prisma) {
+  global.prisma = prisma;
 }
 
 export default prisma;
